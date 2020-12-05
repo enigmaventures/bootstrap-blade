fb6b4104d483f24f83fbb947b1db1fb42a33501f
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 14:38:55 +0800

    Remove CardDeck component's render method

--- src/Components/CardDeck.php
+++ src/Components/CardDeck.php
@@ -10,14 +10,4 @@ class CardDeck extends BootstrapComponent
      * @var string
      */
     public string $class = 'card-deck';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::card-deck');
-    }
 }
