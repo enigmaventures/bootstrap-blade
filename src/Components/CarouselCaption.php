51e831c6e679b1a6238585903cf6f6280510ad18
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 16:55:54 +0800

    Remove CarouselCaption component's render method

--- src/Components/CarouselCaption.php
+++ src/Components/CarouselCaption.php
@@ -10,14 +10,4 @@ class CarouselCaption extends BootstrapComponent
      * @var string
      */
     public string $class = 'carousel-caption d-none d-md-block';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::carousel-caption');
-    }
 }
