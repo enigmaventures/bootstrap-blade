2193b335727a464eb67a9cf58b84b46dca5d5a0a
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 16:52:37 +0800

    Update ButtonToolbar component's render method

--- src/Components/ButtonToolbar.php
+++ src/Components/ButtonToolbar.php
@@ -23,6 +23,6 @@ class ButtonToolbar extends BootstrapComponent
             'aria-label' => 'Button toolbar'
         ]);
 
-        return view('bootstrap-blade::button-toolbar');
+        return view()->first(["bootstrap-blade::button-group", "bootstrap-blade::default"]);
     }
 }
