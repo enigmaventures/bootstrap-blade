0dd096ac81dcfd9d3993dd6748251a49bf6ba41d
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 19:33:29 +0800

    Remove DropdownDivider component's render method

--- src/Components/DropdownDivider.php
+++ src/Components/DropdownDivider.php
@@ -10,14 +10,4 @@ class DropdownDivider extends BootstrapComponent
      * @var string
      */
     public string $class = 'dropdown-divider';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::dropdown-divider');
-    }
 }
