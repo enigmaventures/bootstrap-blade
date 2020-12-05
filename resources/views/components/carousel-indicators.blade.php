ca74b21e6eaf7e1dccc522548394495e67e05eba
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 15:42:25 +0800

    Delete CarouselIndicators component's blade file

--- resources/views/components/carousel-indicators.blade.php
+++
@@ -1,5 +0,0 @@
-<ol {{ $attributes->merge($componentAttributes()) }}>
-    @foreach (range(0, $items - 1) as $item)
-    <li data-target="{{ $target }}" data-slide-to="{{ $loop->index }}"@if ($loop->first) class="active"@endif/>
-    @endforeach
-</ol>
