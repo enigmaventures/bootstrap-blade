5f0a9fc638b9628b7d132e94262bb947a66eb3d5
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 14:32:12 +0800

    Remove Card component's render method

--- src/Components/Card.php
+++ src/Components/Card.php
@@ -10,14 +10,4 @@ class Card extends BootstrapComponent
      * @var string
      */
     public string $class = 'card';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::card');
-    }
 }
