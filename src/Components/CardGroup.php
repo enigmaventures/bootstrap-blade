6fe3387bd57933dac791590e325c915b01667e98
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 14:43:40 +0800

    Remove CardGroup component's render method

--- src/Components/CardGroup.php
+++ src/Components/CardGroup.php
@@ -10,14 +10,4 @@ class CardGroup extends BootstrapComponent
      * @var string
      */
     public string $class = 'card-group';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::card-group');
-    }
 }
