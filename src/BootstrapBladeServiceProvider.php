a12f3231340794f79d52c006bf74167022253181
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 15:51:34 +0800

    Remove CarouselInner from service provider

--- src/BootstrapBladeServiceProvider.php
+++ src/BootstrapBladeServiceProvider.php
@@ -40,7 +40,6 @@ class BootstrapBladeServiceProvider extends ServiceProvider
             Components\CardTitle::class,
             Components\Carousel::class,
             Components\CarouselCaption::class,
-            Components\CarouselInner::class,
             Components\CarouselItem::class,
             Components\CarouselNext::class,
             Components\CarouselPrevious::class,
