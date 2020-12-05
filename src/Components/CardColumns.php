f76ee4c46e3e5757cb21c1f73894a3d38a553b5c
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 14:37:06 +0800

    Remove CardColumns component's render method

--- src/Components/CardColumns.php
+++ src/Components/CardColumns.php
@@ -10,14 +10,4 @@ class CardColumns extends BootstrapComponent
      * @var string
      */
     public string $class = 'card-columns';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::card-columns');
-    }
 }
