55a13fed3f2b5a1ebb113508c2ab0d85e375892d
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 14:58:21 +0800

    Remove CardSubtitle component's render method

--- src/Components/CardSubtitle.php
+++ src/Components/CardSubtitle.php
@@ -10,16 +10,4 @@ class CardSubtitle extends BootstrapComponent
      * @var string
      */
     public string $class = 'card-subtitle';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        parent::render();
-
-        return view('bootstrap-blade::card-subtitle');
-    }
 }
