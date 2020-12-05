06f276826f2879b724d6e857927eb580d61d5781
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 17:07:22 +0800

    Delete CustomRadio component's blade file

--- resources/views/components/custom-radio.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="{{ $containerClass }}">
-    <input {{ $attributes->merge($componentAttributes()) }}/>
-    <label for="{{ $id }}" class="{{ $labelClass }}">{{ $label }}</label>
-    {{ $slot }}
-</div>
