128fb26358285331a4b983d3d8c84ddabb80016e
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 14:40:46 +0800

    Remove CardFooter component's render method

--- src/Components/CardFooter.php
+++ src/Components/CardFooter.php
@@ -10,14 +10,4 @@ class CardFooter extends BootstrapComponent
      * @var string
      */
     public string $class = 'card-footer';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::card-footer');
-    }
 }
