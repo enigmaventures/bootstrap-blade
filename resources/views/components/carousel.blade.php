eda7d69db315e5c734bd02bb73e3c4520098849e
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 16:14:48 +0800

    Update Carousel component's view file

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -12,11 +12,14 @@
     </div>
 
     @if ($controls)
-    <a class="carousel-control-prev" href="{{ $hash($id) }}">
+    <a class="carousel-control-prev" href="{{ $hash($id) }}" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
 
-    <x-carousel-next :href="$hash($id)"/>
+    <a class="carousel-control-next" href="{{ $hash($id) }}" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
     @endif
 </div>
