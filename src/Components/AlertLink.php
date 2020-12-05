481f78cee1caaf6a7f3548a65dfb71ad0af9ef0d
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 13:54:01 +0800

    Remove AlertLink component's render method

--- src/Components/AlertLink.php
+++ src/Components/AlertLink.php
@@ -10,16 +10,4 @@ class AlertLink extends Link
      * @var string
      */
     public string $class = 'alert-link';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        parent::render();
-
-        return view('bootstrap-blade::alert-link');
-    }
 }
