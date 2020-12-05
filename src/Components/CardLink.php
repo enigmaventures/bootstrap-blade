9f2b6dbeb6247c00026a4fe1045ee68e697e5791
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 14:56:41 +0800

    Remove CardLink component's render method

--- src/Components/CardLink.php
+++ src/Components/CardLink.php
@@ -10,16 +10,4 @@ class CardLink extends Link
      * @var string
      */
     public string $class = 'card-link';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        parent::render();
-
-        return view('bootstrap-blade::card-link');
-    }
 }
