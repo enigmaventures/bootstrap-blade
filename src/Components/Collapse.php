53060b0393874d0c6fb4ab99010f5335909b047a
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 16:21:34 +0800

    Remove Collapse component's render method

--- src/Components/Collapse.php
+++ src/Components/Collapse.php
@@ -10,14 +10,4 @@ class Collapse extends BootstrapComponent
      * @var string
      */
     public string $class = 'collapse';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::collapse');
-    }
 }
