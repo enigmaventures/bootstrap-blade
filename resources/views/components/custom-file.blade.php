c3c069b64aa153361d4c46cad0b27bf8f551535f
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 16:48:57 +0800

    Delete CustomFile component's blade file

--- resources/views/components/custom-file.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="{{ $containerClass }}">
-    <label for="{{ $id }}" class="{{ $labelClass }}">{{ $label }}</label>
-    <input {{ $attributes->merge($componentAttributes()) }}/>
-    {{ $slot }}
-</div>
