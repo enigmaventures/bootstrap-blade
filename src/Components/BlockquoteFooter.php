d40c81f08ba52b3c84ed69a4fbd3f982e1e6afca
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-05 13:58:14 +0800

    Remove BlockquoteFooter component's render method

--- src/Components/BlockquoteFooter.php
+++ src/Components/BlockquoteFooter.php
@@ -10,14 +10,4 @@ class BlockquoteFooter extends BootstrapComponent
      * @var string
      */
     public string $class = 'blockquote-footer';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::blockquote-footer');
-    }
 }
