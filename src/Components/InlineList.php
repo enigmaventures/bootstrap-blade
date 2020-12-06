8c5ba21ff4aabe75f285540ff71db3e6c19b243e
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 20:37:15 +0800

    Remove InlineList component's render method

--- src/Components/InlineList.php
+++ src/Components/InlineList.php
@@ -10,14 +10,4 @@ class InlineList extends ListElement
      * @var string
      */
     public string $class = 'list-inline';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::inline-list');
-    }
 }
