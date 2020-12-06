ec3bb8f3bed9adaea0b007505a7ff470ed38e1f3
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 20:36:12 +0800

    Remove FormRow component's render method

--- src/Components/FormRow.php
+++ src/Components/FormRow.php
@@ -10,14 +10,4 @@ class FormRow extends BootstrapComponent
      * @var string
      */
     public string $class = 'form-row';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::form-row');
-    }
 }
