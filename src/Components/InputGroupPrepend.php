5d0529b9ba955e550ab4cc6ead44eb73b0c55d23
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 20:53:32 +0800

    Remove InputGroupPrepend component's render method

--- src/Components/InputGroupPrepend.php
+++ src/Components/InputGroupPrepend.php
@@ -10,14 +10,4 @@ class InputGroupPrepend extends BootstrapComponent
      * @var string
      */
     public string $class = 'input-group-prepend';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::input-group-prepend');
-    }
 }
