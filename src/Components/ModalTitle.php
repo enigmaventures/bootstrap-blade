89241eef98db90c404e73b16a3ab68f04ac0b494
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 21:29:57 +0800

    Remove ModalTitle component's render method

--- src/Components/ModalTitle.php
+++ src/Components/ModalTitle.php
@@ -10,14 +10,4 @@ class ModalTitle extends BootstrapComponent
      * @var string
      */
     public string $class = 'modal-title';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::modal-title');
-    }
 }
