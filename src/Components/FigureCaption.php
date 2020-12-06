c807dd38be1e28f18db48011bbdc9a189b38ec1d
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 20:11:47 +0800

    Remove FigureCaption component's render method

--- src/Components/FigureCaption.php
+++ src/Components/FigureCaption.php
@@ -10,14 +10,4 @@ class FigureCaption extends BootstrapComponent
      * @var string
      */
     public string $class = 'figure-caption';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::figure-caption');
-    }
 }
