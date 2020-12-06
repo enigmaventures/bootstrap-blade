2a2932d94b02267507b89f698052eb6eef982773
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 20:57:59 +0800

    Remove Label component's render method

--- src/Components/Label.php
+++ src/Components/Label.php
@@ -10,14 +10,4 @@ class Label extends BootstrapComponent
      * @var string
      */
     public string $class = 'col-form-label';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::label');
-    }
 }
