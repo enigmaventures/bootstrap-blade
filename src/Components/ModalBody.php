59957f81651cd931864dffe13054bb03e3aa1dda
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 21:19:40 +0800

    Remove ModalBody component's render method

--- src/Components/ModalBody.php
+++ src/Components/ModalBody.php
@@ -10,14 +10,4 @@ class ModalBody extends BootstrapComponent
      * @var string
      */
     public string $class = 'modal-body';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::modal-body');
-    }
 }
