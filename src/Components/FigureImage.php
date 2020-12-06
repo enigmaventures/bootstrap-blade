77eec97186d1779ea562e453c83cfddadeb6cd00
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 20:12:27 +0800

    Remove FigureImage component's render method

--- src/Components/FigureImage.php
+++ src/Components/FigureImage.php
@@ -10,18 +10,4 @@ class FigureImage extends Image
      * @var string
      */
     public string $class = 'figure-img img-fluid';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        if ($this->thumbnail) {
-            $this->addClass('img-thumbnail');
-        }
-
-        return view('bootstrap-blade::figure-image');
-    }
 }
