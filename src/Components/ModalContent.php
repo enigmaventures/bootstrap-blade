ed8ae89b6eb656898adecd49f9b5145b4ac9e82a
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 21:27:26 +0800

    Remove ModalContent component's render method

--- src/Components/ModalContent.php
+++ src/Components/ModalContent.php
@@ -10,14 +10,4 @@ class ModalContent extends BootstrapComponent
      * @var string
      */
     public string $class = 'modal-content';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::modal-content');
-    }
 }
