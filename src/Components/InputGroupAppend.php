2f48e627cf67d4eeeefeee3ab3f2d1633573a2a3
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 20:52:46 +0800

    Remove InputGroupAppend component's render method

--- src/Components/InputGroupAppend.php
+++ src/Components/InputGroupAppend.php
@@ -10,14 +10,4 @@ class InputGroupAppend extends BootstrapComponent
      * @var string
      */
     public string $class = 'input-group-append';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::input-group-append');
-    }
 }
