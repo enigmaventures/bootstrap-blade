30c9fc66a3868039844ef1d65dd60634fad0796d
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 20:11:33 +0800

    Remove Figure component's render method

--- src/Components/Figure.php
+++ src/Components/Figure.php
@@ -10,14 +10,4 @@ class Figure extends BootstrapComponent
      * @var string
      */
     public string $class = 'figure';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::figure');
-    }
 }
