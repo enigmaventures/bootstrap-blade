a70be23abcf0d4c3d181b1ded4ef4045fdeb98e4
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 20:29:23 +0800

    Remove FormCheck component's render method

--- src/Components/FormCheck.php
+++ src/Components/FormCheck.php
@@ -10,14 +10,4 @@ class FormCheck extends BootstrapComponent
      * @var string
      */
     public string $class = 'form-check';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::form-check');
-    }
 }
