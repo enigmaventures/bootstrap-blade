2456338493a7ff94999af889406dabc6b68157f9
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 21:29:25 +0800

    Remove ModalHeader component's render method

--- src/Components/ModalHeader.php
+++ src/Components/ModalHeader.php
@@ -10,14 +10,4 @@ class ModalHeader extends BootstrapComponent
      * @var string
      */
     public string $class = 'modal-header';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::modal-header');
-    }
 }
