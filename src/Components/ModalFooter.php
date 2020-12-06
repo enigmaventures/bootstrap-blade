27759f5879ba4d8e9894dbc4c44ca4b8b25d95a1
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-06 21:28:55 +0800

    Remove ModalFooter component's render method

--- src/Components/ModalFooter.php
+++ src/Components/ModalFooter.php
@@ -10,14 +10,4 @@ class ModalFooter extends BootstrapComponent
      * @var string
      */
     public string $class = 'modal-footer';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::modal-footer');
-    }
 }
