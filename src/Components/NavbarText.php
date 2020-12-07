2ccccc89a8637957d54f82ce68c910bf60b2fe3f
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-07 20:59:59 +0800

    Remove NavbarText component's render method

--- src/Components/NavbarText.php
+++ src/Components/NavbarText.php
@@ -10,14 +10,4 @@ class NavbarText extends BootstrapComponent
      * @var string
      */
     public string $class = 'navbar-text';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::navbar-text');
-    }
 }
