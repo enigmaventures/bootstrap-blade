c1b8bb4cae6e0935e70714c53edd2f2eb8a9aa50
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-07 20:58:34 +0800

    Remove NavbarCollapse component's render method

--- src/Components/NavbarCollapse.php
+++ src/Components/NavbarCollapse.php
@@ -10,14 +10,4 @@ class NavbarCollapse extends BootstrapComponent
      * @var string
      */
     public string $class = 'collapse navbar-collapse';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::navbar-collapse');
-    }
 }
