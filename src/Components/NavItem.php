8244ef7bbbdb51c139b2de89f286430ebe329a59
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2020-12-11 21:17:49 +0800

    Remove NavItem component's render method

--- src/Components/NavItem.php
+++ src/Components/NavItem.php
@@ -10,18 +10,4 @@ class NavItem extends ListItem
      * @var string
      */
     public string $class = 'nav-item';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        if ($this->active) {
-            $this->addClass('active');
-        }
-
-        return view('bootstrap-blade::nav-item');
-    }
 }
