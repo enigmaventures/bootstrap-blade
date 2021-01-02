697d83d5085ee1abe7a930e679df33a6a61807d0
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2021-01-02 16:22:32 +0800

    Update PageItem component's render method

--- src/Components/PageItem.php
+++ src/Components/PageItem.php
@@ -19,14 +19,9 @@ class PageItem extends ListItem
     public function render()
     {
         if ($this->active) {
-            $this->addClass('active');
             $this->addAriaAttribute('aria-current', 'page');
         }
 
-        if ($this->disabled) {
-            $this->addClass('disabled');
-        }
-
-        return view('bootstrap-blade::page-item');
+        parent::render();
     }
 }
