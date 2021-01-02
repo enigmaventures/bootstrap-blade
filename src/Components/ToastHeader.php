c682688e1452b4334e176dcce5b38197a5501c59
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2021-01-02 18:35:17 +0800

    Remove ToastHeader component's render method

--- src/Components/ToastHeader.php
+++ src/Components/ToastHeader.php
@@ -10,14 +10,4 @@ class ToastHeader extends BootstrapComponent
      * @var string
      */
     public string $class = 'toast-header';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::toast-header');
-    }
 }
