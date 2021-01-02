d2800652a11e0239702a6a73761eb4a39727d357
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2021-01-02 18:36:10 +0800

    Delete UnstyledList component's blade template

--- resources/views/components/unstyled-list.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($ordered)
-<ol {{ $attributes->merge($componentAttributes()) }}>
-    {{ $slot }}
-</ol>
-@else
-<ul {{ $attributes->merge($componentAttributes()) }}>
-    {{ $slot }}
-</ul>
-@endif
