a8ced7166040efe7bc992b8eeffa4feea4d9e3e6
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2021-01-02 16:18:14 +0800

    Remove NavItemLink component's render method

--- src/Components/NavItemLink.php
+++ src/Components/NavItemLink.php
@@ -10,14 +10,4 @@ class NavItemLink extends Link
      * @var string
      */
     public string $class = 'nav-item nav-link';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::nav-item-link');
-    }
 }
