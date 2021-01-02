65de4ea13ce3ee912ac86bf44b69d1ac970e549f
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2021-01-02 18:35:53 +0800

    Remove UnstyledList component's render method

--- src/Components/UnstyledList.php
+++ src/Components/UnstyledList.php
@@ -10,14 +10,4 @@ class UnstyledList extends ListElement
      * @var string
      */
     public string $class = 'list-unstyled';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view('bootstrap-blade::unstyled-list');
-    }
 }
