e0284896de35efd17e2bd1d820a37ddbce88441c
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2021-01-02 16:24:09 +0800

    Remove PageLink component's render method

--- src/Components/PageLink.php
+++ src/Components/PageLink.php
@@ -10,21 +10,4 @@ class PageLink extends Link
      * @var string
      */
     public string $class = 'page-link';
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|\Closure|string
-     */
-    public function render()
-    {
-        $this->addHtmlAttribute('href', $this->href);
-
-        if ($this->disabled) {
-            $this->addAriaAttribute('aria-disabled', 'true');
-            $this->addHtmlAttribute('tabindex', '-1');
-        }
-
-        return view('bootstrap-blade::page-link');
-    }
 }
